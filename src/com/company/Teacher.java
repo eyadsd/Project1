f5994b5b79e302cd43dd119bd09c135909125264
Eyad <eyadsd6@gmail.com>
2018-12-20 23:21:37 +0200

    implement teacher

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -2,5 +2,42 @@ package com.company;
 
 public class Teacher {
     String name;
+    int classesPerDay;
+    int daysPerWeek;
+    int maximumNumberOfLectures;
+    int currentNumberOflectures; // number of lectures for this teacher per week
+    int maximumNumberOfDays;
+    int currentNumberOfDays;
+    boolean [] occupiedDays; // the days that this teacher gives lectures in
+    boolean[][] available;
 
+    public Teacher(boolean[][] available,int maximumNumberOfDays,int maximumNumberOfLectures){
+        currentNumberOflectures = 0;
+        currentNumberOfDays = 0;
+        this.maximumNumberOfDays = maximumNumberOfDays;
+        this.maximumNumberOfLectures = maximumNumberOfLectures;
+        this.available = new boolean[daysPerWeek][classesPerDay];
+        for (int i = 0;i < daysPerWeek;i++)
+        {
+            for ( int j = 0; j<classesPerDay;j++)
+            {
+                this.available[i][j] = available[i][j];
+            }
+        }
+        occupiedDays = new boolean[daysPerWeek];
+    }
+    public boolean assignToPeriod(int dayNumber , int periodNumber,Subject subject){
+        if(available[dayNumber][periodNumber] == false)
+        {
+            if(occupiedDays[dayNumber] == false)
+            {
+                currentNumberOfDays++;
+                occupiedDays[dayNumber] = true;
+
+            }
+            currentNumberOflectures++;
+            return true;
+        }
+        return false;
+    }
 }
